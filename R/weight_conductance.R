bda212bccb129dc26a44ccec630e24302b41cf31
Matthew Kling <mattkling@gmail.com>
2024-04-18 13:18:09 -0400

    documentation cleanup

--- R/weight_conductance.R
+++ R/weight_conductance.R
@@ -5,9 +5,10 @@
 #' which can be used to integrate non-wind factors into a wind connectivity analysis.
 #'
 #' As a hypothetical example, to incorporate a decreased but nonzero likelihood of dispersal over inhospitable areas, \code{w} could be a raster layer with 0.1 indicating water or mountains and 1.0 elsewhere.
+#' This would have the effect of down-weighting conductance over water or mountains by 90%.
 #'
 #' @param rose A raster stack created using \code{windrose_rasters()}.
-#' @param w A raster layer with values to be multiplied by \code{rose}. For example, supplying a layer with 1 for land and 0.1 for water would downweight connectivity over water by 90%. This layer must have the same spatial properties as the windrose raster.
+#' @param w A raster layer with values to be multiplied by \code{rose}. This layer must have the same spatial properties as the windrose raster.
 #' @return A version of \code{rose}, with conductance values weighted by \code{w}.
 #' @export
 weight_conductance <- function(rose, w){
