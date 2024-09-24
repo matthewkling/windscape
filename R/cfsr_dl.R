ce9ccc852370e05d15040563de39599b50289826
Matthew Kling <mattkling@gmail.com>
2024-09-24 15:26:31 -0400

    cfsr_dl hour selection

--- R/cfsr_dl.R
+++ R/cfsr_dl.R
@@ -1,6 +1,7 @@
 
 
-cfsr_dl_day <- function(variable = "wnd10m", year = 1979, month = 1, day = 1,
+cfsr_dl_block <- function(variable = "wnd10m",
+                        year = 1979, month = 1, day = 1, hmin = 0, hmax = 23,
                         xmin = 260, xmax = 270, ymin = 40, ymax = 50){
 
       if(! year %in% 1979:2010) stop("'year' must be between 1979 and 2010")
@@ -25,14 +26,14 @@ cfsr_dl_day <- function(variable = "wnd10m", year = 1979, month = 1, day = 1,
 
       # bounds
       x <- c(xmin, xmax)
-      # x[x<0] <- x[x<0] + 360
       y <- c(ymin, ymax)
       date <- paste0(year, "-",
                      stringr::str_pad(month, 2, "left", 0), "-",
                      stringr::str_pad(day, 2, "left", 0))
-      t <- as.POSIXct(paste0(date, c(" 00:00:00 UTC", " 23:00:00 UTC")), tz = "UTC")
+      t <- as.POSIXct(paste0(date, c(paste0(" ", stringr::str_pad(hmin, 2, "left", 0), ":00:00 UTC"),
+                                     paste0(" ", stringr::str_pad(hmax, 2, "left", 0), ":00:00 UTC"))), tz = "UTC")
 
-      # indices
+            # indices
       btw <- function(data, z) range(which(data <= max(z) & data >= min(z)))
       lon_i <- btw(lon, x)
       lat_i <- btw(lat, y)
@@ -56,8 +57,9 @@ cfsr_dl_day <- function(variable = "wnd10m", year = 1979, month = 1, day = 1,
       yres <- base::diff(sort(y)) / (lat_count - 1)
       extent <- c(x[1] - 0.5 * xres, x[2] + 0.5 * xres,
                   y[1] - 0.5 * yres, y[2] + 0.5 * yres)
-      vr <- terra::rast(aperm(v, c(2, 1, 3)), extent = extent)
-      ur <- terra::rast(aperm(u, c(2, 1, 3)), extent = extent)
+      nd <- 1:length(dim(v)) # 1:2 if only a single hour, else 1:3
+      vr <- terra::rast(aperm(v, c(2, 1, 3)[nd]), extent = extent)
+      ur <- terra::rast(aperm(u, c(2, 1, 3)[nd]), extent = extent)
       names(vr) <- paste("v", timestamp[time_i[1]:time_i[2]])
       names(ur) <- paste("u", timestamp[time_i[1]:time_i[2]])
       return(list(u = ur, v = vr))
@@ -68,7 +70,7 @@ cfsr_dl_day <- function(variable = "wnd10m", year = 1979, month = 1, day = 1,
 #' Download wind data from the Climate Forecast System Reanalysis (CFSR)
 #'
 #' This function downloads hourly CFSR wind data from the NCAR THREDDS server, for one or more historic dates, within a spatial bounding box.
-#' Data for all factorial combinations of 'years', 'months', and 'days' are downloaded and combined.
+#' Data for all factorial combinations of 'years', 'months', 'days', and 'hours' are downloaded and combined.
 #'
 #' @param variable Name of the CFSR wind variable to download. Options include "wnd10m" (the default, representing wind speed
 #'    10 m above the ground), as well as "wnd1000", "wnd850", "wnd700", "wnd500", and "wnd200" (representing higher altitudes,
@@ -76,16 +78,17 @@ cfsr_dl_day <- function(variable = "wnd10m", year = 1979, month = 1, day = 1,
 #' @param years Integer vector representing the historic year(s) for which data are to be accessed, in the range 1979-2010.
 #' @param months Integer vector representing the month(s) for which data are to be accessed, in the range 1-12.
 #' @param days Integer vector representing day(s) for which data is to be requested, in the range 0-31.
+#' @param hlim Vector of length 2, containing integers between 0 and 23, giving the range of hours (in UTC time zone) to download.
 #' @param xlim Vector of length 2, giving longitudinal limits of the data to download, in the range 0 to 360.
 #' @param ylim Vector of length 2, giving latitudinal limits of the data to download, in the range -90 to 90.
 #' @return A list containing u and v components of the wind field, each as a SpatRaster with a layer for each hour of each requested date.
 #' @export
 cfsr_dl <- function(variable = "wnd10m",
                     years = 1979, months = 1, days = 1,
-                    xlim = c(260, 270), ylim = c(40, 50)){
-      q <- expand.grid(variable = variable, day = days, month = months, year = years,
+                    hlim = c(0, 23), xlim = c(260, 270), ylim = c(40, 50)){
+      q <- expand.grid(variable = variable, day = days, month = months, year = years, hmin = min(hlim), hmax = max(hlim),
                        xmin = min(xlim), xmax = max(xlim), ymin = min(ylim), ymax = max(ylim))
-      w <- purrr::pmap(q, cfsr_dl_day)
+      w <- purrr::pmap(q, cfsr_dl_block)
       c(terra::rast(purrr::map(w, "u")),
         terra::rast(purrr::map(w, "v")))
 }
